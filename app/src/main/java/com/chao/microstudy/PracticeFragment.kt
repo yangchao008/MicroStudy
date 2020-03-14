b313e59385ff739ebf47bd9baedb175443bf8c26
hans <18770090820@163.com>
2020-03-14 13:00:06 +0800

    test下

--- app/src/main/java/com/chao/microstudy/PracticeFragment.kt
+++ app/src/main/java/com/chao/microstudy/PracticeFragment.kt
@@ -43,7 +43,7 @@ class PracticeFragment : Fragment(){
             setNewData(texts)
             bindToRecyclerView(recyclerView)
             setOnItemClickListener { _, _, position ->
-                val intent = Intent(activity, DragImageActivity::class.java)
+                val intent = Intent(activity, DetailActivity::class.java)
                 intent.putExtra(DetailActivity.VIEW_ID, position)
                 startActivity(intent)
             }
